10a970905561b4ede4dfd5dca3425577506a8232
Joel Cormack <dgowellkomrak@gmail.com>
2023-07-11 18:11:38 -0600

    feature/sucessfully saving the form data to the google sheet in one row

--- google-sheets-api.php
+++ google-sheets-api.php
@@ -1,7 +1,23 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php'; // Replace with the path to your autoload.php file
+
+putenv('GOOGLE_APPLICATION_CREDENTIALS=./tapacode-customer-survey-e5bb3ef21d23.json'); // Replace with the path to your service account key file
+
+$credentialsPath = getenv('GOOGLE_APPLICATION_CREDENTIALS');
+
+$client = new Google_Client();
+$client->useApplicationDefaultCredentials();
+
+$client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
+
+$service = new Google_Service_Sheets($client);
+
+$spreadsheet_id = '15Mzar6OtUH7kYTtVxbwFosXSETIiVe4POZfoCzTRlFQ'; // Replace with the ID of your Google Sheets spreadsheet
+$range = 'Sheet1!A2:J2'; // Replace with the range of cells you want to insert data into
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Get the values of the form fields
+    // Get the form data from the $_POST superglobal array
     $easeOfUse = $_POST['ease-of-use'];
     $easeOfUseFeedback = $_POST['ease-of-use-feedback'];
     $qualityOfInfo = $_POST['quality-of-info'];
@@ -13,51 +29,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $purchaseDecision = $_POST['purchase-decision'];
     $recommendation = $_POST['recommendation'];
 
-    // Print out the values of the form fields
-    echo "Ease of Use: $easeOfUse\n";
-    echo "Ease of Use Feedback: $easeOfUseFeedback\n";
-    echo "Quality of Information: $qualityOfInfo\n";
-    echo "Quality of Information Feedback: $qualityOfInfoFeedback\n";
-    echo "Source of Awareness: $sourceOfAwareness\n";
-    echo "Customer Category: $customerCategory\n";
-    echo "Consideration of Competition: $considerationOfCompetition\n";
-    echo "Considered Products: $consideredProducts\n";
-    echo "Purchase Decision: $purchaseDecision\n";
-    echo "Recommendation: $recommendation\n";
+
+    $values = [
+        [$easeOfUse, 
+        $easeOfUseFeedback, 
+        $qualityOfInfo, 
+        $qualityOfInfoFeedback, 
+        $sourceOfAwareness, 
+        $customerCategory, 
+        $considerationOfCompetition, 
+        $consideredProducts,
+        $purchaseDecision,
+        $recommendation],
+    ];
+
+    $body = new Google_Service_Sheets_ValueRange([
+        'values' => $values
+    ]);
+
+    $params = [
+        'valueInputOption' => 'RAW'
+    ];
+
+    $result = $service->spreadsheets_values->update($spreadsheet_id, $range, $body, $params);
+
+        printf("Your response have been saved, thank you!");
 }
-
-/*
-require_once __DIR__ . '/vendor/autoload.php'; // Replace with the path to your autoload.php file
-
-putenv('GOOGLE_APPLICATION_CREDENTIALS=/path/to/service-account-key.json'); // Replace with the path to your service account key file
-
-$client = new Google_Client();
-$client->useApplicationDefaultCredentials();
-$client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
-
-$service = new Google_Service_Sheets($client);
-
-$spreadsheet_id = 'your-spreadsheet-id'; // Replace with the ID of your Google Sheets spreadsheet
-$range = 'Sheet1!A1:B2'; // Replace with the range of cells you want to insert data into
-
-// Get the form data from the $_POST superglobal array
-$name = $_POST['name'];
-$email = $_POST['email'];
-
-$values = [
-    [$name, $email],
-];
-
-$body = new Google_Service_Sheets_ValueRange([
-    'values' => $values
-]);
-
-$params = [
-    'valueInputOption' => 'RAW'
-];
-
-$result = $service->spreadsheets_values->update($spreadsheet_id, $range, $body, $params);
-
-printf("%d cells updated.", $result->getUpdatedCells());
-*/
 ?>
